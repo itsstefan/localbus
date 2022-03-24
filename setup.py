3bd5d1e69b4f6d64b4155f4ad7b710e47cb88438
8327 <8327>
2022-03-24 15:31:33 +0000

    up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="localbus",
-    version="0.0.1",
+    version="0.0.2",
     author="",
     author_email="itsstefan@protonmail.com",
     description="Lightweight python IPC package",
